77369d0eb2317ec74d97a73f7f2f8a4713cae0a9
Djzulmat <zkaharov@gmail.com>
2021-12-01 11:30:01 -0800

    Update RockPaperScissors.java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,20 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class RockPaperScissors {
 	
 	public static void rps(String choice1, String choice2) {
 		
-		String[] choices = {"Rock", "Paper", "Scissors"};
+		
+		List<String> choices = new ArrayList<String>(Arrays.asList("Rock", "Paper", "Scissors"));
+		
+		List<String> choicesToLower = choices.stream().map(String :: toLowerCase).collect(Collectors.toList());
+		
 		
 		String result = "";
 		
-		if (choice1.equals(choice2)) {
+		if (choicesToLower.contains(choice1) && choicesToLower.contains(choice2)) {
 			
-			result = "TIE";
+			if (choice1.equals(choice2)) {
+				
+				result = "TIE";
+				
+			} else if ((choice1.equals(choicesToLower.get(0)) && choice2.equals(choicesTolower.get(2).toLowerCase())) || (choice1.equals(choicesToLower.get(1)) && choice2.equals(choicesToLower.get(0))) || (choice1.equals(choicesToLower.get(2)) && choice2.equals(choicesToLower.get(1)))) {
+				
+				result = "Player 1 wins";
+				
+			} else {
+				
+				result = "Player 2 wins";
+				
+			}
 			
-		} else if ((choice1.equals(choices[0].toLowerCase()) && choice2.equals(choices[2].toLowerCase())) || (choice1.equals(choices[1].toLowerCase()) && choice2.equals(choices[0].toLowerCase())) || (choice1.equals(choices[2].toLowerCase()) && choice2.equals(choices[1].toLowerCase()))) {
-			result = "Player 1 wins";
 		} else {
-			result = "Player 2 wins";
+			
+			result = "Invalid input. Either choice1 or choice2 is incorrect. Please check and input correct version";
+			
 		}
 		
 		System.out.println(result);
@@ -22,7 +42,13 @@ public class RockPaperScissors {
 	}
 
 	public static void main(String[] args) {
+		rps("paper", "rock");
+		rps("rock", "paper");
+		rps("paper", "scissors");
 		rps("rock", "rock");
+		rps("scissors", "paper");
+		rps("scissors", "blahhh");
+		
 
 	}
 
